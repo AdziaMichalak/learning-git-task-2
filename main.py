e62a228007d9beed5ac422df3aa9cc1bad75010e
AdziaMichalak <104758585+AdziaMichalak@users.noreply.github.com>
2022-05-23 12:23:22 +0200

    project 1

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ def add(a, b):
 
 add(3, 4)
 add(45, 90)
+print()
+def customized_hello(first_name, last_name):
+    print("Hello Mr %s %s" % (first_name, last_name))
+
+customized_hello("John", "Cleese")
