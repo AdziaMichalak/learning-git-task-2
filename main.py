4b905f724bd306fef19b367033160f1a1fd4258d
Magdalena Michalak <mbawolik80@gmail.com>
2022-05-25 10:55:37 +0200

    changes

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 print("podaj slowo: ")
 word = input()
 
-<<<<<<< HEAD
-add(3, 4)
-add(45, 90)
-print()
-def customized_hello(first_name, last_name):
-    print("Hello Mr %s %s" % (first_name, last_name))
-
-customized_hello("John", "Cleese")
-
-=======
 if str(word) == "".join(reversed(word)): 
   print("Palindrome")
 else: 
     print("Not Palindrome")
->>>>>>> 7208480da0cd627d9bd8a185a5c322f96763e72c
+
